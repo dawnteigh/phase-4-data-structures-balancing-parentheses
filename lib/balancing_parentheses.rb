a0189cfdf1459e4ebaf365e4cf79e12e5de12a40
Donte Ladatto <donte279@gmail.com>
2022-10-21 11:00:24 -0500

    commit tree fbd0036f7e964180d4657bb9ae3b29a27514af05

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,20 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses(str)
+  stack = Stack.new
+  pa = str.chars
+  pa.each{ |p| stack.push(p) }
+  op = pa.select{ |p| p == "(" }.length
+  cl = pa.select{ |p| p == ")" }.length
+  dif = (op - cl).abs
+  if dif == 0
+    if stack.peek == "(" && pa.first == ")"
+      dif += 2
+    elsif stack.peek == "(" || pa.first == ")"
+      dif += 1
+    else
+      dif
+    end
+  end
+  dif
+end
